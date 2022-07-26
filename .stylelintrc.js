7b8499c02ec9fe36c66107976eec0c21a96f8996
Umer Hurramov <umer.hurramov@gmail.com>
2022-07-26 15:08:30 +0300

    Add tailwind css

---
+++ .stylelintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "extends": "stylelint-config-standard",
+  "plugins": ["stylelint-scss"],
+  "rules": {
+    "at-rule-no-unknown": null,
+    "scss/at-rule-no-unknown": true,
+    "no-descending-specificity": null,
+    "rule-empty-line-before": null,
+    "declaration-empty-line-before": null,
+    "block-no-empty": null,
+    "no-missing-end-of-source-newline": null,
+    "number-leading-zero": null,
+    "at-rule-empty-line-before": null,
+    "color-hex-case": null,
+  },
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+Forked from https://staticsiteboilerplate.com/
+
+* Homepage: [http://staticsiteboilerplate.com/](http://staticsiteboilerplate.com//)
+* Documentation: [https://docs.staticsiteboilerplate.com/](https://docs.staticsiteboilerplate.com/)
+
+## Installation
+
+Runs on mac OS node v14.15.0
+
+```bash
+git clone https://github.com/umer4ik/static-site-boilerplate
+ ```
+ 
+ ```bash
+cd static-site-boilerplate
+ ```
+ 
+ ```bash
+rm -rf .git && git init
+ ```
+ 
